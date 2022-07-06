ffde45f0aadbc07486452056a0cc173a1626497c
texa2302 <a.fedorov2302@gmail.com>
2022-07-06 14:00:39 +0300

    grid reafactor

--- src/components/Tickets/tickets.js
+++ src/components/Tickets/tickets.js
@@ -13,21 +13,35 @@ const Tickets = () => {
                     company
                 </div>
             </div>
-            <div className={styles.gridtable}>
-                <div>route</div>
-                <div>time</div>
-                <div>switches</div>
-                <div>touteData</div>
-                <div>timeData</div>
-                <div>switchesData</div>
-            </div>
-            <div className={styles.gridtable}>
-                <div>route</div>
-                <div>time</div>
-                <div>switches</div>
-                <div>touteData</div>
-                <div>timeData</div>
-                <div>switchesData</div>
+            <div className={styles.grid__container}>
+                <div className={styles.gridtable}>
+                    <div className="route">
+                        <div>route</div>
+                        <div>touteData</div>
+                    </div>
+                    <div className="time">
+                        <div>time</div>
+                        <div>timeData</div>
+                    </div>
+                    <div className="stops">
+                        <div>stops</div>
+                        <div>stopsData</div>
+                    </div>
+                </div>
+                <div className={styles.gridtable}>
+                <div className="route">
+                        <div>route</div>
+                        <div>touteData</div>
+                    </div>
+                    <div className="time">
+                        <div>time</div>
+                        <div>timeData</div>
+                    </div>
+                    <div className="stops">
+                        <div>stops</div>
+                        <div>stopsData</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
