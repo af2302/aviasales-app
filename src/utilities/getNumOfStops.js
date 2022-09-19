5d766b73cc449e643445607969dc06579c9e94f7
texa2302 <a.fedorov2302@gmail.com>
2022-09-19 23:48:29 +0300

    number of stops bug fixed

--- src/utilities/getNumOfStops.js
+++ src/utilities/getNumOfStops.js
@@ -1,6 +1,8 @@
 const getNumOfStops = (ticket) =>
   ticket.segments
     .map((element) => element.stops.length)
-    .reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+    .reduce((previousValue, currentValue) => { 
+      return (previousValue > currentValue ? previousValue : currentValue)
+    })
 
 export default getNumOfStops;
